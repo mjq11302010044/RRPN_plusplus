058b09e50c1814b4ff3999502865d19748e50715
mjq11302010044 <mjq11302010044@gmail.com>
2021-11-04 12:33:03 +0800

    newly support pytoch higher verison

--- maskrcnn_benchmark/layers/__init__.py
+++ maskrcnn_benchmark/layers/__init__.py
@@ -4,7 +4,7 @@ from .batch_norm import FrozenBatchNorm2d
 from .misc import Conv2d
 from .misc import ConvTranspose2d
 from .misc import interpolate
-from .nms import nms
+#from .nms import nms
 from .roi_align import ROIAlign
 from .rroi_align import RROIAlign
 from .roi_align_rotated import ROIAlignRotated as ROIAlignRotatedFromD2
@@ -19,7 +19,7 @@ from .mish_activation import Mish
 from .transformer.model import make_model as make_transformer
 from .transformer.model_transformer import Transformer
 
-__all__ = ["nms", "roi_align", "ROIAlign", "roi_pool", "ROIPool",
+__all__ = ["roi_align", "ROIAlign", "roi_pool", "ROIPool",
            "weighted_smooth_l1_loss", "smooth_l1_loss", "Conv2d", "ConvTranspose2d", "interpolate",
            "FrozenBatchNorm2d", "RROIAlign", "ROIAlignRotatedFromD2", "ROIAlignRotatedKeep", "Mish", "Transformer"
-           "transformer_loss"]
+           "transformer_loss"] #"nms",
--- maskrcnn_benchmark/layers/nms.py
+++ maskrcnn_benchmark/layers/nms.py
@@ -2,6 +2,6 @@
 # from ._utils import _C
 from maskrcnn_benchmark import _C
 
-nms = _C.nms
+nms = None #_C.nms
 # nms.__doc__ = """
 # This function performs Non-maximum suppresion"""
